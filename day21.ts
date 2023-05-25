ff11a6454b486dc923f5a08909c5ad3110e724af
Siddharth9890 <siddharth.a9890@gmail.com>
2023-05-25 09:56:16 +0530

    added day 21 question chunk array

---
+++ day21.ts
@@ -0,0 +1,5 @@
+function chunk(arr: any[], size: number): any[][] {
+    return Array.from({ length: Math.ceil(arr.length / size) }, function(_: any, index: number) {
+    return arr.slice(index * size, index * size + size);
+  });
+};
